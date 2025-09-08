cff050282ffbc9fffa08c9c830e78341f705c5d1
jlim0255 <140394484+J4CKL1M@users.noreply.github.com>
2025-09-08 11:16:35 +0800

    Refactored the prefix "/api/notes from server.js
    Use router. (REST API) instead of calling them in server.js

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -4,31 +4,19 @@ const router = express.Router();
 
 // to modularise the endpoint, we refactor to this pattern using router
 router.get("/", (req, res) => {
-    res.status(200).send("you have 20 notes");
+    res.status(200).send("You just fetched the notes");
+})
+
+router.post("/", (req, res) => {
+    res.status(200).send("Note created successfully");
+})
+
+router.put("/:id", (req, res) => {
+    res.status(200).json({message: "Note updated successfully!"})
+})
+
+router.delete("/:id", (req, res) => {
+    res.status(200).json({message: "Note deleted successfully!"})
 })
 
 export default router;
-
-// What is endpoint
-// a combination of URL + HTTP method that lets 
-// the client interact with a specific resource
-
-// app.get("/api/notes", (req, res) => {
-//     res.status(200).send("you have 20 notes");
-// })
-
-
-// app.post("/api/notes", (req, res) => {
-//     // 201 means something created successfully
-//     // use send/ json (send in json)
-//     res.status(201).json({message: "post created successfully!"})
-// })
-
-
-// app.put("/api/notes/:id", (req, res) => {
-//     res.status(200).json({message: "post updated successfully!"})
-// })
-
-// app.delete("/api/notes/:id", (req, res) => {
-//     res.status(200).json({message: "post deleted successfully!"})
-// })
