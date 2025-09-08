387e6551e4743e3c7181b0d890c345dfd62500af
jlim0255 <229141621+jlim0255-workship@users.noreply.github.com>
2025-09-08 12:42:21 +0800

    Add functionality for the get and post requests
    Added functionality for get and post notes
    
    Allow users to retrieve all notes from the db, and allow users to create a new note to the db, with all content details listed out.
    
    Tested functionality with POSTMAN, its working

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,12 +1,46 @@
 // two ways to doing this: 
 // 1) the arrow function way
-export const getAllNotes = (req, res) => {
-    res.status(200).send("You just fetched the notes");
-}
+// export const getAllNotes = (req, res) => {
+//     res.status(200).send("You just fetched the notes");
+// }
 
 // 2) the normal function way
-export function createNote(req, res) {
-    res.status(200).send("Note created successfully");
+import Note from "../models/Note.js"
+
+export async function getAllNotes(req, res) {
+    try{
+        const notes = await Note.find()
+        res.status(200).json(notes)
+
+    }catch(error){
+        console.log("Error in getAllNotes controller", error)
+        res.status(500).json({message: "Internal server error"})
+
+    }
+    
+}
+
+export async function createNote(req, res) {
+    // res.status(200).send("Note created successfully");
+    try{
+        // TODO: why the below does not work?
+        // const {user_title, user_content} = req.body
+        // const newNote = new Note({title: user_title, content: user_content})
+
+        const {title, content} = req.body
+
+        // another way of putting content in the newly created note
+        // const newNote = new Note({title, content})
+        // await newNote.save()
+        // res.status(201).json({message:"Note created successfully!"})
+
+        const note = new Note({title, content})
+        const savedNote = await note.save()        
+        res.status(201).json(savedNote)
+    } catch(error){
+        console.log("Error in createNote controller", error)
+        res.status(500).json({message: "Internal server error"})
+    }
 }
 
 export function updateNote(req, res){
--- backend/src/server.js
+++ backend/src/server.js
@@ -15,6 +15,9 @@ const PORT = process.env.PORT || 5001
 
 connectDB();
 
+//middleware
+app.use(express.json());
+
 app.use("/api/notes", notesRoutes);
 
 
