ace5e39d13194546b8c922a2b25ca5c62e1d2c73
jlim0255 <229141621+jlim0255-workship@users.noreply.github.com>
2025-09-09 23:24:25 +0800

    HomePage Done
    as title

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors"
 
 // comnfig the whole .env file
 dotenv.config()
@@ -15,9 +16,13 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 //middleware
+app.use(cors({
+    origin: "http://localhost:5173",
+})) // allow request from UI
 app.use(express.json()); //middleware to parse req.body
 app.use(rateLimiter); //middleware to rate limit
 app.use("/api/notes", notesRoutes);// middleware to call controllers in endpoints
+
 
 // modified: connect the DB first, then listen to the server
 connectDB().then( () => {app.listen(PORT, () => {
