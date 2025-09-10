19937b76d23b2fb4d66f00960a66d3f8ec84c91d
jlim0255 <229141621+jlim0255-workship@users.noreply.github.com>
2025-09-10 14:00:00 +0800

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,8 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors"
+import path from "path"
+
 
 // comnfig the whole .env file
 dotenv.config()
@@ -15,13 +17,35 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5001
 
+// create a variable (source of backend)
+const __dirname = path.resolve()
+
 //middleware
-app.use(cors({
+// only do this in development
+if (process.env.NODE_ENV !== "production"){
+    app.use(cors({
     origin: "http://localhost:5173",
-})) // allow request from UI
+    })) // allow request from UI
+}
+
+
 app.use(express.json()); //middleware to parse req.body
 app.use(rateLimiter); //middleware to rate limit
 app.use("/api/notes", notesRoutes);// middleware to call controllers in endpoints
+
+// only do this in production
+if (process.env.NODE_ENV === "production"){
+    // serve the optimized react app (frontend dist) as a static assest
+    // go one layer up to frontend to get dist
+    // make the front end alive and built
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+
+    // when they visit any route, just give the respective route in front end view
+    app.get("*", (req, res) =>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 
 // modified: connect the DB first, then listen to the server
