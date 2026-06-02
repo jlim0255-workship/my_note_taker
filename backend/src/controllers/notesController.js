fd1607f9d8bb10949eeeb78eb510bfdef401cfd1
jlim0255-workship <jlim0255.work@gmail.com>
2026-06-02 22:20:47 +0800

    clean up unused chunks

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -12,9 +12,6 @@ import {sql} from "../config/db.js"
 // RETRIEVE ALL NOTES
 export async function getAllNotes(req, res) {
     try{
-        // const notes = await Note.find().sort({createdAt: -1}) // .-1 newest first, 1 older first
-        // res.status(200).json(notes)
-
         const notes = await sql`SELECT * FROM notes ORDER BY created_at DESC`
         res.status(200).json(notes)
 
@@ -30,10 +27,6 @@ export async function getAllNotes(req, res) {
 export async function getNoteById(req, res) {
     // get id from the URL if there is
     try{
-        // const note = await Note.findById(req.params.id)
-        // if (!note) return res.status(404).json({message: "Note not found"})            
-        // res.status(200).json(note)
-
         const note = await sql`SELECT * FROM notes WHERE id = ${req.params.id}`
 
         if (note.length === 0) return res.status(404).json({message: "Note not found"})
@@ -52,22 +45,7 @@ export async function getNoteById(req, res) {
 export async function createNote(req, res) {
     // res.status(200).send("Note created successfully");
     try{
-        // TODO: why the below does not work?
-        // const {user_title, user_content} = req.body
-        // const newNote = new Note({title: user_title, content: user_content})
-
         const {title, content} = req.body
-
-        // old way
-        // const newNote = new Note({title, content})
-        // await newNote.save()
-        // res.status(201).json({message:"Note created successfully!"})
-        
-        // another way of putting content in the newly created note
-        // with better clarity
-        // const note = new Note({title, content})
-        // const savedNote = await note.save()
-        // res.status(201).json(savedNote)
 
         const newNote = await sql`INSERT INTO notes (title, content) VALUES (${title}, ${content}) RETURNING *`
         res.status(201).json({data: newNote[0], message: "Note created successfully!"})
@@ -85,21 +63,6 @@ export async function updateNote(req, res){
         // ONLY UPDATE the params passed in (still works if we just pass the title or content)
         const {title, content} = req.body
 
-        //we called it id because it is id in the put request in notesRoutes.js
-        // update the new title and content, based on the id
-
-        // old way
-        // await Note.findByIdAndUpdate(req.params.id, {title, content})
-
-        // better way
-        // const updatedNote = await Note.findByIdAndUpdate(req.params.id, {title, content})
-        
-        // // leave message if note not found
-        // if (!updatedNote) return res.status(404).json({message:"Note not found!"})
-        
-        // // leave successful message
-        // res.status(200).json({message: "Note updated successfully"})
-
         const updatedNote = await sql`UPDATE notes SET title = ${title}, content = ${content} WHERE id = ${req.params.id} RETURNING *`
         if (updatedNote.length === 0) return res.status(404).json({message: "Note not found!"})
         res.status(200).json({ data: updatedNote[0], message: "Note updated successfully" })
@@ -114,14 +77,6 @@ export async function updateNote(req, res){
 export async function deleteNote(req, res){
     // res.status(200).json({message: "Note deleted successfully!"})
     try{
-        // // only need the note id to delete that note        
-        // const updatedNote = await Note.findByIdAndDelete(req.params.id)
-        
-        // // leave message if note not found
-        // if (!updatedNote) return res.status(404).json({message:"Note not found!"})
-        
-        // // leave successful message
-        // res.status(200).json({message: "Note deleted successfully"})
 
         const deletedNote = await sql`DELETE FROM notes WHERE id = ${req.params.id} RETURNING *`
         if (deletedNote.length === 0) return res.status(404).json({message: "Note not found!"})
