3ef0a8f227d4e541ee498e521ce8693a02787faf
jlim0255-workship <jlim0255.work@gmail.com>
2026-06-03 11:32:45 +0800

    added indexer function in the existing controllers
    no await, send response to user while the indexing process is doing in the background

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -7,6 +7,7 @@
 // 2) the normal function way
 import Note from "../models/Note.js"
 import {sql} from "../config/db.js"
+import {indexNote} from "../services/noteIndexer.js"
 
 
 // RETRIEVE ALL NOTES
@@ -50,6 +51,8 @@ export async function createNote(req, res) {
         const newNote = await sql`INSERT INTO notes (title, content) VALUES (${title}, ${content}) RETURNING *`
         res.status(201).json({data: newNote[0], message: "Note created successfully!"})
 
+        indexNote(newNote[0].id, `${newNote[0].title} ${newNote[0].content}`); // fire and forget, no await because we don't need to wait for it to finish to respond to the user. If it fails, we'll catch the error inside the indexNote function and update the embedding_status accordingly
+
     } catch(error){
         console.log("Error in createNote controller", error)
         res.status(500).json({message: "Internal server error"})
@@ -66,6 +69,8 @@ export async function updateNote(req, res){
         const updatedNote = await sql`UPDATE notes SET title = ${title}, content = ${content} WHERE id = ${req.params.id} RETURNING *`
         if (updatedNote.length === 0) return res.status(404).json({message: "Note not found!"})
         res.status(200).json({ data: updatedNote[0], message: "Note updated successfully" })
+        
+        indexNote(updatedNote[0].id, `${updatedNote[0].title} ${updatedNote[0].content}`); // re-index the note with the updated content. Again, fire and forget, no await
 
     }catch(error){
         console.log("Error in updateNote controller", error)
