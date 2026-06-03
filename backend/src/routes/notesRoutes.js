255a4724b37873251fee2e5e6bb15608d51d640b
jlim0255-workship <jlim0255.work@gmail.com>
2026-06-03 11:51:27 +0800

    new routes for graph, place it before get note id, else it will be swallowed by get note id /:id pattern

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { createNote, deleteNote, getAllNotes, getNoteById, updateNote } from "../controllers/notesController.js";
+import { getGraph } from "../controllers/graphController.js";
 
 // create a router object
 const router = express.Router();
 
 // to modularise the endpoint, we refactor to this pattern using router
 router.get("/", getAllNotes)
+router.get("/graph", getGraph) // get request to fetch the graph data (nodes and edges)
 router.get("/:id", getNoteById)
 router.post("/", createNote) // post request to create a new note
 router.put("/:id", updateNote) // put request to update a note
